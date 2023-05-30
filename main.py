022a0bf5060238f8c12f7ab9d5b95a7dfdd78c3b
hyun0229 <dhgustjr29@naver.com>
2023-05-30 16:28:48 +0900

    add

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from datetime import datetime
-
+import  random
 import openai
 import pymysql
 
 conn = pymysql.connect(host='sudosoft-1.cpf9r06qfgdc.us-east-2.rds.amazonaws.com', user='user', password='chocho0804', db='crontab_ex1', charset='utf8') #DB open
 cur = conn.cursor() #커서
 
-openai.api_key = "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"
+openai.api_key = "[api-key]"
 
 def get_chat_response(messages):
 
@@ -19,12 +19,14 @@ def get_chat_response(messages):
     return answer
 
 
-
-
-user_input = "서울은 지금 몇시야?"
+a= ["지진","홍수","화재","산사태"]
+b=random.choice(a)+' 메뉴얼 알려줘'
+user_input = b
 
 messages = [
-    {"role": "system", "content": "Disaster manual supporter"},
+    {"role": "system", "content": "You are a Disaster manual assistant, name is C-DGM"},
+    {"role": "user", "content": "What is C-DGM?"},
+    {"role": "assistant", "content": "That is my name, Chat bot - Disaster Guide Manual"},
     {"role": "user", "content": user_input}
 ]
 
@@ -37,5 +39,6 @@ cur.execute(sql, vals)
 conn.commit()
 
 print("C-DGM:", chat_response)
+
 cur.close()
 conn.close()
